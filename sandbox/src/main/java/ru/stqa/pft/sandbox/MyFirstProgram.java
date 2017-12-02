972e11d112e5cb4565fd548d829651bd07509a90
MaxVarEmreis <34187948+MaxVarEmreis@users.noreply.github.com>
2017-12-02 20:31:01 +0100

    Added few functions

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,9 +1,35 @@
 package ru.stqa.pft.sandbox;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    hello("world");
+    hello("user");
+    hello("Alexei");
+
+    double l = 5;
+    System.out.println("Powierzchnia o boku " + l + " = " + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Powierzchnia prostokąta o boku " + a + " i " + b + " = " + area(a, b));
+
   }
 
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double len) {
+    return len * len;
+
+  }
+
+  public static double area(double a, double b) {
+
+
+    return a * b;
+  }
 }
