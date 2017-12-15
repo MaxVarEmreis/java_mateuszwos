29935e6c9fb3d68ae451863eca3c01e64575320a
MaxVarEmreis <34187948+MaxVarEmreis@users.noreply.github.com>
2017-12-15 19:22:36 +0100

    Homework nr 8 v2
    Added optimisation in ContactCreationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -21,6 +21,10 @@ public class NavigationHelper extends HelperBase {
     }
 
   public void gotoContactPage() {
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Edit / add address book entry")) {
+      return;
+    }
     click(By.linkText("add new"));
   }
 
