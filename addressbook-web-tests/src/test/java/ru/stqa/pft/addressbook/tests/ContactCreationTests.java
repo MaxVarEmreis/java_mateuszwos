e9367bd70055a43b91e2b1d58859b4d39c421f30
MaxVarEmreis <34187948+MaxVarEmreis@users.noreply.github.com>
2018-01-10 18:56:52 +0100

    Module 8, lesson 5 - hash for groups and contacts

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -17,8 +17,8 @@ public class ContactCreationTests extends TestBase {
     ContactData contact = new ContactData().withFirtsName("test_first_name").withLastName("test_last_name").withMobile("test_mobile").withEmail("test_email").withGroup("test1");
     app.goTo().contactPage();
     app.contact().create(contact);
+    assertThat(app.contact().count(),  equalTo(before.size() + 1));
     Contacts after = app.contact().all();
-    assertThat(after.size(),  equalTo(before.size() + 1));
     assertThat(after, equalTo(
             before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
   }
