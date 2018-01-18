f9454fb50911dfb7c476d72de4fa47a6d6150f49
MaxVarEmreis <34187948+MaxVarEmreis@users.noreply.github.com>
2018-01-18 16:21:10 +0100

    Module 5, lesson 6 for contacts

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -5,7 +5,10 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -16,15 +19,16 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ContactCreationTests extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validContacts() {
+  public Iterator<Object[]> validContacts() throws IOException {
     List<Object[]> list = new ArrayList<Object[]>();
-    File photo = new File("src/test/resources/stru.png");
-    list.add(new Object[]{new ContactData().withFirtsName("firtsName 1").withLastName("lastName 1")
-            .withMobile("mobile 1").withEmail("email 1").withPhoto(photo)});
-    list.add(new Object[]{new ContactData().withFirtsName("firtsName 2").withLastName("lastName 2")
-            .withMobile("mobile 2").withEmail("email 2").withPhoto(photo)});
-    list.add(new Object[]{new ContactData().withFirtsName("firtsName 3").withLastName("lastName 3")
-            .withMobile("mobile 3").withEmail("email 3").withPhoto(photo)});
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+    String line = reader.readLine();
+    while (line !=null) {
+      String[] split = line.split(";");
+      list.add(new Object[]{new ContactData().withFirtsName(split[0]).withLastName(split[1]).withMobile(split[2])
+              .withEmail(split[3])});
+      line = reader.readLine();
+    }
     return list.iterator();
   }
 
