ae4ebe2a2567eef5ec7cf8f435717f1380efb915
MaxVarEmreis <34187948+MaxVarEmreis@users.noreply.github.com>
2018-01-22 19:44:09 +0100

    Module 2, lesson 7

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -3,18 +3,35 @@ package ru.stqa.pft.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Entity;
+import org.hibernate.annotations.Table;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
 import java.util.Objects;
 
 @XStreamAlias("group")
+@javax.persistence.Entity
+@javax.persistence.Table(name = "group_list")
 public class GroupData {
   @XStreamOmitField
+  @Id
+  @Column(name = "group_id")
   private int id = Integer.MAX_VALUE;
+
   @Expose
+  @Column(name = "group_name")
   private String name;
+
   @Expose
+  @Column(name = "group_header")
+  @Type(type = "text")
   private String header;
+
   @Expose
+  @Column(name = "group_footer")
+  @Type(type = "text")
   private String footer;
 
 
